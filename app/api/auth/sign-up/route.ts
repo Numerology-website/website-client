136ed6bd1761697be600cd3359c76cd1247a1a5d
thanhquang28 <thanhquang2805@gmail.com>
2023-10-01 14:07:01 +0700

    feat: forgot password screen

--- app/api/auth/sign-up/route.ts
+++
@@ -1,4 +0,0 @@
-export async function POST(request: Request) {
-  console.log(request);
-  //   const url = process.env.NEXT_PUBLIC_API_URL + path;
-}
--- components/auth/ForgotPasswordForm.tsx
+++ components/auth/ForgotPasswordForm.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { firebaseAuth } from "@/libs/firebase"
 import { toastify } from "@/libs/toastify"
+import { countryCode, getFlagEmoji } from "@/utils/countryCode"
 import { PostAPI } from "@/utils/fetch"
 import { getErrorMessage } from "@/utils/helpers"
 import {
@@ -8,17 +9,19 @@ import {
   RecaptchaVerifier,
   signInWithPhoneNumber,
 } from "firebase/auth"
+import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 type ForgotPasswordStep = "INPUT_PHONE_NUMBER" | "VERIFY_OTP"
 
 export const ForgotPasswordForm = () => {
-  const [step, setStep] = useState<ForgotPasswordStep>("INPUT_PHONE_NUMBER")
-  const [phoneNumber, setPhoneNumber] = useState<string>("+840123456789")
+  const [step, setStep] = useState<ForgotPasswordStep>("VERIFY_OTP")
+  const [phoneNumber, setPhoneNumber] = useState<string>("")
+  const [country_code, setCountryCode] = useState<string>("+84")
   const [otp, setOtp] = useState<string>("")
   const [result, setResult] = useState<ConfirmationResult | undefined>(
-    undefined
+    undefined,
   )
   const [newPassword, setNewPassword] = useState<string>("")
   const [confirmNewPassword, setConfirmNewPassword] = useState<string>("")
@@ -32,7 +35,7 @@ export const ForgotPasswordForm = () => {
     try {
       const isValidPhoneNumber = await PostAPI<boolean>({
         url: "/auth/check-phone-number",
-        body: { phone_number: phoneNumber },
+        body: { phone_number: country_code + phoneNumber },
         headers: {
           API_PUBLIC_KEY: process.env.NEXT_PUBLIC_API_PUBLIC_KEY,
         },
@@ -53,7 +56,8 @@ export const ForgotPasswordForm = () => {
     const isValid = await checkPhoneNumber()
     if (!isValid) return
     const appVerifier = window.recaptchaVerifier
-    signInWithPhoneNumber(firebaseAuth, phoneNumber, appVerifier)
+    const phone_number = country_code + phoneNumber
+    signInWithPhoneNumber(firebaseAuth, phone_number, appVerifier)
       .then((confirmationResult) => {
         setResult(confirmationResult)
         setStep("VERIFY_OTP")
@@ -125,48 +129,131 @@ export const ForgotPasswordForm = () => {
       "forgot-password-form",
       {
         size: "invisible",
-      }
+      },
     )
   }, [])
   return (
-    <>
+    <div className="flex flex-col items-center gap-2">
       {step === "INPUT_PHONE_NUMBER" && (
-        <div>
-          <input
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-          <div id="forgot-password-form"></div>
-          <button onClick={submitPhoneNumber}>Submit</button>
+        <div className="flex flex-col gap-7">
+          <div className="flex gap-2">
+            <div className="w-fit">
+              <select
+                id="countries"
+                className="bg-auth-input-gray block w-full rounded-md border  text-sm text-white"
+                style={{ backgroundColor: "rgba(243, 246, 249, 0.36)" }}
+                onChange={(e) => setCountryCode(e.target.value)}
+              >
+                <option value="+84">{getFlagEmoji("VN")} - Vietnam +84</option>
+                {countryCode.map((country, index) => (
+                  <option key={index} value={country.dial_code}>
+                    {getFlagEmoji(country.code)} - {country.name}{" "}
+                    {country.dial_code}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="w-full">
+              <input
+                className="bg-auth-input-gray text h-10 w-full rounded-md border-0 border-gray-400 p-2 px-8 py-4 text-xs !text-white placeholder-white opacity-90"
+                type={"text"}
+                placeholder={"Số điện thoại của bạn"}
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+            </div>
+            <div id="forgot-password-form"></div>
+          </div>
+          <div className="flex justify-center gap-5">
+            <button
+              className="rounded-full border border-solid border-gray-400 px-10 py-2 text-xs hover:bg-white hover:text-[#3F4254]"
+              onClick={submitPhoneNumber}
+            >
+              Gửi
+            </button>
+            <Link
+              className="rounded-full border border-solid border-gray-400 px-10 py-2 text-xs hover:bg-white hover:text-[#3F4254]"
+              href="/login"
+            >
+              Huỷ
+            </Link>
+          </div>
         </div>
       )}
       {step === "VERIFY_OTP" && (
-        <div>
-          <label>PhoneNumber</label>
-          <input type="text" value={phoneNumber} disabled />
-          <label>OTP</label>
+        <div className="flex flex-col gap-2">
+          <div id="forgot-password-form"></div>
+          <div className="flex gap-2">
+            <div className="w-fit">
+              <select
+                id="countries"
+                className="bg-auth-input-gray block w-full rounded-md border  text-sm text-white"
+                style={{ backgroundColor: "rgba(243, 246, 249, 0.36)" }}
+                disabled
+              >
+                <option value="+84">{getFlagEmoji("VN")} - Vietnam +84</option>
+                {countryCode.map((country, index) => (
+                  <option key={index} value={country.dial_code}>
+                    {getFlagEmoji(country.code)} - {country.name}{" "}
+                    {country.dial_code}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="w-full">
+              <input
+                className="bg-auth-input-gray text h-10 w-full rounded-md border-0 border-gray-400 p-2 px-8 py-4 text-xs !text-white placeholder-white opacity-90"
+                type={"text"}
+                placeholder={"Số điện thoại của bạn"}
+                value={phoneNumber}
+                disabled
+              />
+            </div>
+          </div>
+          <div className="w-full">
+            <input
+              className="bg-auth-input-gray text h-10 w-full rounded-md border-0 border-gray-400 p-2 px-8 py-4 text-xs !text-white placeholder-white opacity-90"
+              type="text"
+              inputMode="numeric"
+              autoComplete="one-time-code"
+              value={otp}
+              placeholder="Nhập mã nhận được"
+              onChange={(e) => setOtp(e.target.value)}
+            />
+          </div>
           <input
-            type="text"
-            inputMode="numeric"
-            autoComplete="one-time-code"
-            value={otp}
-            onChange={(e) => setOtp(e.target.value)}
-          />
-          <label>New Password</label>
-          <input
+            className="bg-auth-input-gray text h-10 w-full rounded-md border-0 border-gray-400 p-2 px-8 py-4 text-xs !text-white placeholder-white opacity-90"
             type="password"
+            placeholder="Nhập mật khẩu mới"
             value={newPassword}
             onChange={(e) => setNewPassword(e.target.value)}
           />
-          <label>Confirm New Password</label>
           <input
+            className="bg-auth-input-gray text h-10 w-full rounded-md border-0 border-gray-400 p-2 px-8 py-4 text-xs !text-white placeholder-white opacity-90"
             type="password"
+            placeholder="Nhập lại mật khẩu mới"
             value={confirmNewPassword}
             onChange={(e) => setConfirmNewPassword(e.target.value)}
           />
-          <button onClick={validateOTP}>Submit</button>
+          <div className="mt-5 flex justify-center gap-7">
+            <button
+              className="rounded-full border border-solid border-gray-400 px-10 py-2 hover:bg-white hover:text-[#3F4254]"
+              onClick={validateOTP}
+            >
+              Submit
+            </button>
+            <button
+              className="rounded-full border border-solid border-gray-400 px-10 py-2 hover:bg-white hover:text-[#3F4254]"
+              type="button"
+              onClick={() => router.push("/login")}
+            >
+              Huỷ
+            </button>
+          </div>
         </div>
       )}
-    </>
+    </div>
   )
 }
