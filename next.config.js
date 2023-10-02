30f06f30e01b70b154559f0afdf487f6fce501ff
thanhquang28 <thanhquang2805@gmail.com>
2023-10-02 21:54:59 +0700

    feat: add landing pages using htm files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => [
+    {
+      source: "/than-so-hoc-tron-doi",
+      destination: "/landing-pages/than-so-hoc-tron-doi.htm",
+    },
+  ],
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
