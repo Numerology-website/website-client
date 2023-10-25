dd61d9ac9f9cb838ee9d20e8a02b5d4cc0e9c8ed
thanhlq <thanh.lq@zinza.com.vn>
2023-10-25 14:54:24 +0700

    feat: add api edit profile

--- app/services/users/user.service.ts
+++ app/services/users/user.service.ts
@@ -1,10 +1,39 @@
-import { TNumerologyRecord } from "@/interfaces/numerology-records.interface"
 import { IUsers } from "@/interfaces/users.interface"
-import { GetAPI } from "@/utils/fetch"
-
+import { GetAPI, PostAPI } from "@/utils/fetch"
+type TChangeNameForm = {
+  first_name: string
+  last_name: string
+}
 export const UserService = {
   async myProfile() {
     const response = await GetAPI<IUsers>("/users/me")
     return response
+  },
+  async changePassword(password: string, accessToken: string): Promise<string> {
+    try {
+      return await PostAPI<string>({
+        url: "/users/password",
+        method: "PATCH",
+        accessToken,
+        body: { password },
+      })
+    } catch (error) {
+      throw error
+    }
+  },
+  async changeName(
+    { first_name, last_name }: TChangeNameForm,
+    accessToken: string,
+  ): Promise<IUsers> {
+    try {
+      return await PostAPI<IUsers>({
+        url: "/users/me",
+        method: "PATCH",
+        accessToken,
+        body: { first_name, last_name },
+      })
+    } catch (error) {
+      throw error
+    }
   },
 }
